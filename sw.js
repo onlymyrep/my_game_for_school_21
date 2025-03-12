f4444f0e3261c6b9f451a8ce190e45bb6da6e23c
onlymyrep <tfgryujhgh@gmail.com>
2025-03-12 20:46:53 +0300

    game

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'emoji-puzzle-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/bg.mp3',
+    '/bg.ogg',
+    '/icon-192x192.png',
+    '/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
